6f516e45b418c5745c1a89cfa075e57c8c59464f
rafalpieniazek <rkpieniazek@gmail.com>
2017-10-15 13:03:28 +0200

    Init

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rigidBody;
+	public float speed;
+
+	void Start() {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		float moveHorizont = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizont, 0, moveVertical);
+		rigidBody.AddForce (movement * speed);
+
+	}
+}
