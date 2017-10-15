1e32d06cd2516ab1487466a50dd04409a6b35aa6
rafalpieniazek <rkpieniazek@gmail.com>
2017-10-15 15:24:30 +0200

    colliders, ui view

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
-	private Rigidbody rigidBody;
+	
 	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rigidBody;
+	private int count;
 
 	void Start() {
 		rigidBody = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText (); 
+		winText.text = "";
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	void FixedUpdate() {
@@ -21,6 +29,20 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizont, 0, moveVertical);
 		rigidBody.AddForce (movement * speed);
+	}
 
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString(); 
+		if (count >= 8) {
+			winText.text = "You won!";
+		}
 	}
 }
